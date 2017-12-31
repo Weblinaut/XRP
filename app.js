d76b6b3575c862e4e994d7ba8f25f1f67a2e00bd
Derek <weblinaut@gmail.com>
2017-12-31 12:23:33 -0500

    added some things

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     function getPrice() {
         var xrpPrice = $("#xrpPrice");
-        $.getJSON('//data.ripple.com/v2/exchange_rates/XRP/BTC+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B', function() {
+        $.getJSON('//data.ripple.com/v2/exchange_rates/XRP/USD+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B', function() {
         }).done(function(data) {
             if (xrpPrice.text() !== data.rate) {
                 console.log('updating...')
@@ -9,13 +9,50 @@ $(document).ready(function() {
                 xrpPrice.addClass('highlight');
                 setTimeout(function() {
                     xrpPrice.removeClass('highlight');
-                }, 2500)
+                }, 2000)
             }
             pollXrp();
         }).fail(function() {
             console.log( "error. Will try again soon");
             xrpPrice.text("Failed to retrieve Market Value...Please Wait...Attempting reload");
             pollXrp();
+        });
+
+        var xrpBTC = $("#xrpBTC");
+
+        $.getJSON('//data.ripple.com/v2/exchange_rates/XRP/BTC+rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B', function() {
+        }).done(function(data) {
+            if (xrpBTC.text() !== data.rate) {
+                console.log('updating...');
+                xrpBTC.text(data.rate);
+                xrpBTC.addClass('highlight');
+                setTimeout(function() {
+                    xrpBTC.removeClass('highlight');
+                }, 2500)
+            }
+        }).fail(function() {
+            console.log( "error. Will try again soon");
+            xrpBTC.text("Failed to retrieve Market Value...Please Wait...Attempting reload");
+        });
+
+        var btcPrice = $("#btcPrice");
+
+        $.getJSON('//api.coindesk.com/v1/bpi/currentprice.json', function() {
+        }).done(function(data) {
+            console.log(data.bpi.USD.rate);
+            if (btcPrice.text() !== data.bpi.USD.rate) {
+                console.log('updating...');
+                btcPrice.text(data.bpi.USD.rate);
+                btcPrice.addClass('highlight');
+                setTimeout(function() {
+                    btcPrice.removeClass('highlight');
+                }, 2500)
+            }
+
+        }).fail(function() {
+            console.log( "error. Will try again soon");
+            btcPrice.text("Failed to retrieve Market Value...Please Wait...Attempting reload");
+
         });
     }
 
